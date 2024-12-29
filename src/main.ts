586c0ddb86eb5ced8be9ac187bdf36f7d8aa3058
ZinMinKhant-Alfred <zinminkhant.alfred@gmail.com>
2024-12-29 18:12:39 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-import * as cors from "cors";
 import * as dotenv from "dotenv";
 
 dotenv.config();
@@ -8,9 +7,8 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS for all origins (you can customize this for specific origins)
   app.enableCors({
-    origin: "https://matchable.netlify.app/",
+    origin: "https://matchable.netlify.app",
     methods: "GET, POST, PUT, DELETE",
     allowedHeaders: "Content-Type, Authorization",
   });
